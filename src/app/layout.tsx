53f364be32edfb405de42f05290628e264e581f8
Priyesh Soni <priyeshsoni@zeltatech.com>
2024-11-25 09:40:46 +0530

    app-background-chagnes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/CommonComponent/Header/Header";
 import Footer from "@/components/CommonComponent/Footer/Footer";
-
+import styles from "./page.module.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${styles.body} ` }>
         <Header/>
         {children}
         <Footer/>
