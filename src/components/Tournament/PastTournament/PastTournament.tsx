c93ada6865c5340e7fcc72334b2994df81f42f87
Priyesh Soni <priyeshsoni@zeltatech.com>
2024-12-16 17:03:33 +0530

    added marquee on the home-page

--- src/components/Tournament/PastTournament/PastTournament.tsx
+++ src/components/Tournament/PastTournament/PastTournament.tsx
@@ -2,12 +2,11 @@
 import Image from "next/image";
 import styles from "./pasttournament.module.scss";
 import config from "../../../../config";
-import Link from "next/link";
-import { TournamentData } from "@/services/GameServices";
 import { useRouter } from "next/navigation";
 
 const PastTournament = ({ tournamentList, pastTournament }: any) => {
   if (!tournamentList?.length || pastTournament) return;
+
   return (
     <div className={styles.main_container}>
       {tournamentList?.map((cardValue: any) => (
@@ -129,67 +128,3 @@ export const TournamentCard = ({ cardValue, pastTournament }: any) => {
     </div>
   );
 };
-
-// <div className={styles.gameCard_container} key={cardValue?._id}>
-//   <div className={styles.image_container}>
-//     <Image
-//       src={cardValue?.game?.backgroundImage}
-//       alt="game-logo"
-//       fill
-//       priority
-//     />
-//     <Image
-//       src={cardValue?.thumbnail}
-//       className={styles.thumbnail}
-//       width={100}
-//       height={43}
-//       alt="Play Button"
-//     />
-//   </div>
-//   <div className={styles.tournament_headers}>
-//     <div className={styles.tournamentCardPast__topContent}>
-//       <div className={styles.tournamentCardPast__tourEnded}>
-//         <p>
-//           Tournament <br /> Ended
-//         </p>
-//       </div>
-//       <div className={styles.tournamentCardPast__reward}>
-//         <Image
-//           src={`${config.imageDomain}/Assets/boxT.webp`}
-//           alt="Game Fi"
-//           height={0}
-//           width={0}
-//           sizes="100vw"
-//           style={{
-//             height: "100px",
-//             width: "auto",
-//             marginLeft: "-60px",
-//           }}
-//         />
-//         <p>{cardValue?.reward?.toFixed(2)}</p>
-//         <Image
-//           src={`${config.imageDomain}/loading-images/matic1.webp`}
-//           alt="Gaming Arcade"
-//           height={0}
-//           width={0}
-//           sizes="100vw"
-//           style={{ height: "20px", width: "auto" }}
-//         />
-//       </div>
-//     </div>
-//     <div className={styles.tournament_details}>
-//       <span>
-//         <p>Entry Price</p>
-//         <p>{cardValue?.entryFee}m</p>
-//       </span>
-//       <span>
-//         <p>Players</p>
-//         <p>{cardValue?.players}</p>
-//       </span>
-//       <span>
-//         <p>TOURNAMENT</p>
-//         <p>#{cardValue?.serial}</p>
-//       </span>
-//     </div>
-//   </div>
-// </div>
