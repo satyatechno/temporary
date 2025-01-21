bec9647b82c7f0b5fc6c0b32d968139eea52e551
Priyesh Soni <priyeshsoni@zeltatech.com>
2025-01-21 12:20:49 +0530

    update the key of accessToken

--- src/components/CommonComponent/Header/Header.tsx
+++ src/components/CommonComponent/Header/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const accessToken = searchParams.get("userToken");
+  const accessToken = searchParams.get("accessToken");
 
   const noHeaderFooterPaths = ["/userwallet"];
 
