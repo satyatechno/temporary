971ece471fa01d1186fc97279ee35338df968044
Satyam Sen <satyamsen@Satyams-MacBook-Pro.local>
2025-01-24 15:31:59 +0530

    games issue fix

--- src/app/games/FetchGameServer.ts
+++ src/app/games/FetchGameServer.ts
@@ -1,21 +1,20 @@
-'use server'
+'use server';
 
-import config from "../../../config";
+import config from '../../../config';
 
 export async function fetchGames() {
-    const baseUrl = config.baseURL;
-    try {
-      const response = await fetch(`${baseUrl}games`, {
-        cache: "force-cache",
-      }); // Use `no-store` to avoid caching if needed
-      if (!response.ok) {
-        throw new Error("Failed to fetch home data");
-      }
-      const data = await response.json();
-      return data?.data;
-    } catch (error) {
-      console.error("Error fetching home data:", error);
-      return null;
+  const baseUrl = config.baseURL;
+  try {
+    const response = await fetch(`${baseUrl}games`, {
+      // cache: "force-cache",
+    }); // Use `no-store` to avoid caching if needed
+    if (!response.ok) {
+      throw new Error('Failed to fetch home data');
     }
+    const data = await response.json();
+    return data?.data;
+  } catch (error) {
+    console.error('Error fetching home data:', error);
+    return null;
   }
-  
+}
