0b0d15f7f1fc624ae2f96c796fe6c3e13f52dd63
Priyesh Soni <priyeshsoni@zeltatech.com>
2025-02-04 10:33:39 +0530

    add the loadingImage on first pre-render

--- src/app/games/[game]/page.tsx
+++ src/app/games/[game]/page.tsx
@@ -38,6 +38,8 @@ export async function generateStaticParams() {
     return games.map((game: any) => (
       {
       game: game.name.toString(), 
+      loadingImage:game.loadingImage,
+      thumbnail:game.thumbnail
     }));
   } catch (error) {
     console.error("Error fetching games for static params:", error);
